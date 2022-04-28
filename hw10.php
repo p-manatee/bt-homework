4c3d92b8ed5a88a7bc2683b4ba2d6c2ad0e24ae7
Petar Trajkovic <peromax@gmail.com>
2022-04-28 18:27:18 +0200

    zadaci na dva nacina

--- hw10.php
+++ hw10.php
@@ -63,3 +63,46 @@ printf("Zbir brojeva %d, %d i %d je : %d ",$a,$b,$c,$a+$b+$c);
 echo "</div>";
 
 // End of Task 3
+
+
+
+//Sa Funkcijama za prvi zadatak ne bih znao koje funkcije za matematicke operacije mislim rucno mnozenje, deljenje i ostalo zavrsava posao
+
+//TASK 2 ... uzecu rucno dva random broja umesto mt_rand funkcije i uradicu sa if else posto sam u prethodnom slucaju uradio sa case switch
+
+$rand2 = 4;
+echo '<div style="border:2px solid gray;padding:20px;width:50%;margin:20px auto"">';
+echo "<h2>Zadatak 2 sa if else</h2>";
+if($rand2 == 0){
+    echo "Danas je Nedelja!";
+}else if($rand2 == 1){
+    echo "Danas je Ponedeljak!";
+}
+else if($rand2 == 2){
+    echo "Danas je Utorak!";
+}
+else if($rand2 == 3){
+    echo "Danas je Sreda!";
+}
+else if($rand2 == 4){
+    echo "Danas je Cetvrtak!";
+}
+else if($rand2 == 5){
+    echo "Danas je Petak!";
+}
+else if($rand2 == 6){
+    echo "Danas je Subota!";
+}
+echo "</div>";
+
+//Task 3 iskoristicu mt_rand posto sam u prethodnom primeru koristio hard cod vrednosti a za ispis cu raditi rucno posto sam koristio printf u prethodnom primeru
+$x= mt_rand(1,10);
+$y= mt_rand(1,10);
+$z= mt_rand(1,10);
+$sum2= $x+$y+$z;
+echo '<div style="border:2px solid gray;padding:20px;width:50%;margin:20px auto"">';
+echo "<h2>Zadatak 3 sa echo i mt_rand</h2>";
+
+echo "Zbir brojeva $x, $y i $z je $sum2";
+
+echo "</div>";
